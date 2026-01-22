ac08b18f32d8ab4ec6387721076c6f2c2d7f4c7d
whisperdoll <aliceminacarter@gmail.com>
2026-01-21 18:47:59 -0800

    lets be real for a sec

--- src/components/battleDeck.tsx
+++ src/components/battleDeck.tsx
@@ -15,10 +15,11 @@ export default function BattleDeck(props: Props) {
   return (
     <div className="relative flex min-h-0 min-w-0 flex-col">
       <Card
+        containerProps={{
+          className: "min-h-0 justify-center",
+        }}
         cardProps={{ className: "min-h-0 min-h-0 h-full" }}
         back
-        containerClassName={`min-h-0`}
-        align="center"
       />
       <div className="absolute bottom-0 left-0 w-full bg-white bg-opacity-75 px-1">
         {props.label} ({props.deck?.length || 0})
